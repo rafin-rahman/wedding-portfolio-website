f9f61e9724a4b43a50e1b8fe80d72b9591a6532a
rafin <ra-fin@live.com>
2024-06-05 15:55:19 +0100

    gallery UI updated

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,31 +1,33 @@
+import Image from "next/image";
+
 const files = [
   {
     title: "Wedding",
-    source: "images/gallery/wedding_1.jpg",
+    source: "/images/gallery/wedding_1.jpg",
   },
   {
     title: "Mehendi",
-    source: "images/gallery/mehendi_1.jpg",
+    source: "/images/gallery/mehendi_1.jpg",
   },
   {
     title: "Nikkah",
 
-    source: "images/gallery/nikkah_1.jpg",
+    source: "/images/gallery/nikkah_1.jpg",
   },
   {
     title: "Engagements",
 
-    source: "images/gallery/engagement_1.jpg",
+    source: "/images/gallery/engagement_1.jpg",
   },
   {
     title: "Corporate",
 
-    source: "images/gallery/corporate_1.jpg",
+    source: "/images/gallery/corporate_1.jpg",
   },
   {
     title: "Interviews",
 
-    source: "images/gallery/interview_1.jpg",
+    source: "/images/gallery/interview_1.jpg",
   },
 ];
 
@@ -36,12 +38,13 @@ export default function Gallery() {
       className="grid  gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
     >
       {files.map((file) => (
-        <li key={file.source} className="relative">
-          <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-            <img
+        <li key={file.source} className=" ">
+          <div className=" relative h-60 group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-amber-300 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+            <Image
               src={file.source}
               alt=""
-              className="pointer-events-none object-cover group-hover:opacity-75"
+              className="pointer-events-none object-cover group-hover:opacity-75 "
+              fill
             />
             <button
               type="button"
