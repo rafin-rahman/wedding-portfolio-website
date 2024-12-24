1d404201aaf0dc0356eae94af3205999077247fb
rafin <ra-fin@live.com>
2024-12-24 17:20:54 +0000

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
-// middleware.ts
 import {NextResponse} from "next/server";
 import type {NextRequest} from "next/server";
-import companyConfigs, {defaultBrandKey} from "./utils/company.config";
+import {defaultBrandKey} from "./utils/company.config";
 
 export function middleware(request: NextRequest) {
     const host = request.headers.get("host");
